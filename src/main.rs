aa60b7b38eda984e86a1bf265d5344085e154336
Nikolay Danailov <frostblooded@yahoo.com>
2025-08-10 17:28:29 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(actix_files::Files::new("/", "./static")
+                .index_file("index.html")
+                .use_last_modified(true)
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
