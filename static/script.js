4afe1509aefd0518ccddb1e65f09f51a51ea2c0e
Nikolay Danailov <frostblooded@yahoo.com>
2025-08-12 13:57:34 +0300

    Add filtering for already used guesses to the autocomplete and store already used guesses in local storage

--- static/script.js
+++ static/script.js
@@ -1,23 +1,71 @@
 window.onload = function() {
-    const newGuessInput = document.querySelector("#new-guess-input");
-
-    const _autoCompleteJs = new autoComplete({
+    new autoComplete({
         selector: "#new-guess-input",
-        placeHolder: "Search for Food...",
+        placeHolder: "Guess a cat...",
         data: {
-            src: ["Cookie", "Ati", "Oreo", "Puhche", "Eddie", "Todorka", "Kiara", "Murlin"]
+            src: ["Cookie", "Ati", "Oreo", "Puhche", "Eddie", "Todorka", "Kiara", "Murlin"],
+            filter: filterOutGuesses,
+        },
+        resultsList: {
+            element: handleResultsList,
+            noResults: true,
         },
         resultItem: {
             highlight: true,
         },
         events: {
             input: {
-                selection: (event) => {
-                    const selectedValue = event.detail.selection.value;
-                    console.log(selectedValue);
-                    newGuessInput.value = "";
-                }
+                selection: handleSelection,
             }
         }
     });
+}
+
+const LOCAL_STORAGE_USED_GUESSES_STRING = "usedGuesses";
+
+function handleSelection(event) {
+    const newGuessInput = document.querySelector("#new-guess-input");
+    const selectedValue = event.detail.selection.value;
+    newGuessInput.value = "";
+    addUsedGuess(selectedValue);
+}
+
+function handleResultsList(list, data) {
+    if (!data.results.length) {
+        const message = document.createElement("div");
+        message.setAttribute("class", "no_result");
+        message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+        list.prepend(message);
+    }
+}
+
+function filterOutGuesses(list) {
+    // Filter out already used guesses
+    let usedGuesses = getUsedGuesses();
+    if(usedGuesses === null)
+        return list;
+
+    return list.filter((item) => {
+        return !usedGuesses.includes(item.value);
+    });
+}
+
+function addUsedGuess(guess) {
+    let usedGuesses = getUsedGuesses();
+
+    if(usedGuesses === null) {
+        usedGuesses = [guess];
+    } else {
+        usedGuesses.push(guess);
+    }
+
+    localStorage.setItem(LOCAL_STORAGE_USED_GUESSES_STRING, JSON.stringify(usedGuesses));
+}
+
+function getUsedGuesses() {
+    let retrievedValue = localStorage.getItem(LOCAL_STORAGE_USED_GUESSES_STRING);
+    if(retrievedValue === null)
+        return null;
+
+    return JSON.parse(retrievedValue);
 }
